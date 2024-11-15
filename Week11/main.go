67d7165482f116b6727f21ad492cccebe1f093cb
maziar <sojodyan@gmail.com>
2024-11-15 14:11:37 -0500

    update main.go

--- Week11/main.go
+++ Week11/main.go
@@ -1,3 +1,17 @@
 package main
 
-func main(){}
+import( "fmt")
+func Add(a, b int) int {
+	return a + b
+}
+func Subtract(a, b int) int {
+	return a - b 
+}
+func Multiply(a, b int) int {
+	return a * b
+}
+func main(){
+	fmt.Println("Addition: ", Add(2024 , 1))
+	fmt.Println("Subtraction: ", Subtract(2, 2))
+	fmt.Println("Multiplication: ", Multiply(2, 8))
+}
