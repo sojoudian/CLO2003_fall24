bd128ad73fe589d4b1f34e4f1988af17231128f2
maziar <sojodyan@gmail.com>
2024-11-22 14:19:53 -0500

    add enableCors and fix save IP

--- Week12/main.go
+++ Week12/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"time"
 )
 
 func enableCros(w *http.ResponseWriter) {
@@ -12,9 +14,21 @@ func enableCros(w *http.ResponseWriter) {
 }
 
 func saveIPHandler(w http.ResponseWriter, r *http.Request) {
+	enableCros(&w)
+	if r.Method != "POST" {
+		http.Error(w, "Only POST method is allowed!", http.StatusMethodNotAllowed)
+	}
+	var data map[string]interface{}
+	err := json.NewDecoder(r.Body).Decode(&data)
+	if err != nil {
+		http.Error(w, "Error parsing the JSON request!", http.StatusBadRequest)
+	}
+	currentTime := time.Now().Format("2006-01-02 15:04:05`")
+	data["time"] = currentTime
 
 }
 func main() {
+
 	fs := http.FileServer(http.Dir("./templates"))
 	http.Handle("/", fs)
 	http.HandleFunc("/api/saveIP", saveIPHandler)
